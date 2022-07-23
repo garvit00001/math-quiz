5091c18325f60afeb071b40a4a1822e5c8d62a1a
garvit00001 <96773364+garvit00001@users.noreply.github.com>
2022-07-23 12:50:10 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,24 @@
+player1_name=localStorage.getItem("player1_name");
+player2_name=localStorage.getItem("player2_name");
+
+player1_score=0;
+player2_score=0;
+
+document.getElementById("player1_name").innerHTML=player1_name+":";
+document.getElementById("player2_name").innerHTML=player2_name+":";
+
+document.getElementById("player1_score").innerHTML=player1_score;
+document.getElementById("player2_score").innerHTML=player2_score;
+
+document.getElementById("player_question").innerHTML="Question Turn - "+player1_name;
+document.getElementById("player_answer").innerHTML="Answer Turn - "+player2_name;
+
+function send(){
+    get_number=document.getElementById("number1").value;
+    
+    console.log(get_number);
+    get_number2=document.getElementById("number2").value;
+    
+    console.log(get_number2);
+
+}
